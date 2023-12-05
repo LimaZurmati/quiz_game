9ce8421ac943459d700f144ed68d1cf9037d44f4
LimaZurmati <urzalalima@gmail.com>
2023-12-05 19:11:42 +0000

    add for for options

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 print("Wellcome to quiz game !!")
 score = 0
-queation_num = 0
+question_num = 0
 
 plying = input('Do you want to play ?').lower()
 
 if plying == "yes":
-    queation_num += 1
+    question_num += 1
 
 questions = ("What is another word for 'capability'? ",
              "What is the synonym of the word 'suitable'? ",
@@ -14,7 +14,7 @@ questions = ("What is another word for 'capability'? ",
              "Choose the word which means the opposite of 'beautiful'? ")
 
 options = (("A.abilty","B.disability","C.weak","D.strong"),
-           ("A.qualified","B.prope","C.A and b","none"),
+           ("A.qualified","B.prope","C.A and b","D.none"),
            ("A.care","B.careless","C.notice","D.A and B"),
            ("A.smart","B.clever","C.lazy","D.A and C"),
            ("A.ugly","B.pretty","C.handsome","D.none"))
@@ -27,3 +27,6 @@ for question in questions:
     print("*********************************")
     print("---------------------------------")
     print(question)
+    for option in options[question_num]:
+        print(option)
+
