c4083f9d08f3d20ac5316643ec1f574637d77ae0
LimaZurmati <urzalalima@gmail.com>
2023-12-05 18:40:42 +0000

    add tupple for options

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
 print("Wellcome to quiz game !!")
 score = 0
 queation_num = 0
+
 plying = input('Do you want to play ?').lower()
+
 if plying == "yes":
     queation_num +=
+
 questions = ("What is another word for 'capability': ",
              "What is the synonym of the word 'suitable': ",
              "Find the word which has the same meaning as'attention': ",
              "What is another word which means the same as 'intelligent': ",
              "Choose the word which means the opposite of 'beautiful': ")
+
+options = (("A.abilty","B.disability","C.weak","D.strong"),
+           ("A.qualified","B.prope","C.A and b","none"),
+           ("A.care","B.careless","C.notice","D.A and B"),
+           ("A.smart","B.clever","C.lazy","D.A and C"),
+           ("A.ugly","B.pretty","C.handsome","D.none"))
