8997ee69adcbadf1ee2358a6692c531adcd357ff
LimaZurmati <urzalalima@gmail.com>
2023-12-05 18:21:42 +0000

    add tupple for questions with 5 questions

--- run.py
+++ run.py
@@ -3,4 +3,9 @@ score = 0
 queation_num = 0
 plying = input('Do you want to play ?').lower()
 if plying == "yes":
-    queation_num +=1
+    queation_num +=
+questions = ("What is another word for 'capability': ",
+             "What is the synonym of the word 'suitable': ",
+             "Find the word which has the same meaning as'attention': ",
+             "What is another word which means the same as 'intelligent': ",
+             "Choose the word which means the opposite of 'beautiful': ")
