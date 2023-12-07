cbb84c5019bed441a17acc3eadf2784791de56a5
LimaZurmati <urzalalima@gmail.com>
2023-12-07 17:23:51 +0000

    add fo loop to show the answer as result

--- run.py
+++ run.py
@@ -4,14 +4,12 @@ print("Wellcome to quiz game !!")
 questions = ("What is another word for 'capability'? ",
              "What is the synonym of the word 'suitable'? ",
              "Find the word which has the same meaning as'attention'? ",
-             "What is another word which means the same as 'intelligent'? ",
-             "Choose the word which means the opposite of 'beautiful'? ")
+             "What is another word which means the same as 'intelligent'? ")
 
 options = (("A.abilty","B.disability","C.weak","D.strong"),
            ("A.qualified","B.prope","C.A and b","D.none"),
            ("A.care","B.careless","C.notice","D.A and B"),
-           ("A.smart","B.clever","C.lazy","D.A and C"),
-           ("A.ugly","B.pretty","C.handsome","D.none"))
+           ("A.smart","B.clever","C.lazy","D.A and C"))
 
 answers = ("A","C","D","B")
 gusses = []
@@ -37,4 +35,18 @@ for question in questions:
        print(f"{answers[question_num]} is the correct answer!")
 
     question_num += 1
+
+#Outcome
+print("               Let's see the Outcome (:                  ")
+print("---------------------------------")
+
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("gusses: ", end="")
+for guess in gusses:
+    print(guess, end=" ")
+print()
 
